fca00f785e00ab33bc324cae080a87d27b5354b0
kgabrielnowak <k.gabriel.nowak@gmail.com>
2017-11-20 19:10:49 +0100

    Right after 2 lesson Android Basic: Multiscreen Apps

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,12 +17,66 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
+
+        String words[] = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
+
+        ArrayList<word> numbers = new ArrayList<word>();
+        numbers.add(new word("one","lutti"));
+        numbers.add(new word("two","otiiko"));
+        numbers.add(new word("three","tolookosu"));
+        numbers.add(new word("four","oyyisa"));
+        numbers.add(new word("five","massokka"));
+        numbers.add(new word("six","temmokka"));
+        numbers.add(new word("seven","kenekaku"));
+        numbers.add(new word("eight","kawinta"));
+        numbers.add(new word("nine","wo'e"));
+        numbers.add(new word("ten","na'aacha"));
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+
+        // display a single word.
+
+        WordAdapter adapter = new WordAdapter(this, numbers);
+
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_listyout file.
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -17,12 +17,52 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
+
+        ArrayList<word> phrases = new ArrayList<word>();
+        phrases.add(new word("Where are you going?","minto wuksus"));
+        phrases.add(new word("What is your name?","tinnә oyaase'nә"));
+        phrases.add(new word("My name is...","oyaaset..."));
+        phrases.add(new word("How are you feeling?","michәksәs?"));
+        phrases.add(new word("I’m feeling good.","kuchi achit"));
+        phrases.add(new word("Are you coming?","әәnәs'aa?"));
+        phrases.add(new word("Yes, I’m coming.","hәә’ әәnәm"));
+        phrases.add(new word("I’m coming.","әәnәm"));
+        phrases.add(new word("Let’s go.","yoowutis"));
+        phrases.add(new word("Come here.","әnni'nem"));
+
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+
+        // display a single word.
+
+        WordAdapter adapter = new WordAdapter(this, phrases);
+
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_list.xml file.
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+
+        listView.setAdapter(adapter);
     }
 }
